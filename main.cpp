3203f9092806f057fdd7db5671a46a626acbc3f5
ChenJie <1037003920@qq.com>
2024-03-08 13:51:17 +0800

    2024.3.7

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main()
 	LClabuladong solution;
 	vector<string> d1_arr;
 	vector<int> d2_str;
-	vector<int> d1_arr2 = { 7,12,9,8,9,15 };
+	vector<int> d1_arr2 = { 7,12,9,9,15 };
 	vector<string> d1_arr3 = { "one.two.three","four.five","six" };
 	vector<vector<int>> d2_arr = {{0, 6, 7}, {0, 1, 2}, {1, 2, 3}, {1, 3, 3}, {6, 3, 3}, {3, 5, 1}, {6, 5, 1}, {2, 5, 1}, {0, 4, 5}, {4, 6, 2}};
 	string str1 = "ABFCACDB", str2 = "2";
